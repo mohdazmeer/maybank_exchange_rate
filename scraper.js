756e9d3b705d79aee85ecfc47b0c4847a76a2c87
Mohd Azmeer <mohd.azmeer.omar@gmail.com>
2016-11-07 12:40:11 +0800

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -44,9 +44,10 @@ function run(db) {
 	fetchPage("http://www.maybank2u.com.my/mbb_info/m2u/public/treasuryRates.do?channelId=BTRE-Treasury&cntTypeId=0&cntKey=BTRE04.01&programId=BTRE04-Rates&chCatId=/mbb/Business/BTRE-Treasury", function (body) {
 		// Use cheerio to find things in the page with css selectors.
 		var $ = cheerio.load(body);
-
+		console.log('in body');
 		var elements = $("table.even tbody tr.alt_even td").each(function () {
 			var value = $(this).text().trim();
+			console.log('valuey : ' + value);
 			updateRow(db, value);
 		});
 
